37ba70b885f8acff2c6404bec6f11b8512e29d9b
jcronanuw <jcronan@uw.edu>
2016-06-28 11:44:38 -0700

    Add script and input file
    Testing GitHub -- adding script and input file.

---
+++ test_script.R
@@ -0,0 +1,19 @@
+
+setwd("C:/Users/jcronan/Documents/GitHub/FDM/")
+input_file <- read.table("input_file.csv", header=TRUE, 
+                     sep=",", na.strings="NA", dec=".", strip.white=TRUE)
+
+aa <- vector()
+system.time({
+  for(i in 1:100000)
+  {
+    aa[i] <- 1 + min(0,aa[i-1]) 
+  }
+})
+
+dt <- Sys.Date()
+tm <- format(Sys.time(), format = "%H.%M.%S", 
+             tz = "", usetz = FALSE)
+
+write.csv(aa, file = paste("output_file_on_",dt,"_",tm,".csv",sep = ""), 
+          quote = TRUE, eol = "\n", na = "NA", row.names = FALSE)
